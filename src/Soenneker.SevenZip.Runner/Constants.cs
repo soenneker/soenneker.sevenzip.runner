34502f822c626a9c1e8376017b60a7fb6b6e9af2
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:37:32 -0500

    xml commenting

--- src/Soenneker.SevenZip.Runner/Constants.cs
+++ src/Soenneker.SevenZip.Runner/Constants.cs
@@ -1,7 +1,16 @@
 ﻿namespace Soenneker.SevenZip.Runner;
 
+/// <summary>
+/// Represents the constants.
+/// </summary>
 public static class Constants
 {
+    /// <summary>
+    /// The file name.
+    /// </summary>
     public const string FileName = "7za.exe";
+    /// <summary>
+    /// The library.
+    /// </summary>
     public const string Library = "Soenneker.Libraries.SevenZip";
 }
